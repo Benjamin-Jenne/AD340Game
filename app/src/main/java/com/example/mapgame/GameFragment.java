f3592d7015a740dc0df16b40e56ec81abe9e97b4
benjenne <@Castle27>
2020-05-15 12:10:14 -0700

    I messed up the merge again

--- app/src/main/java/com/example/mapgame/GameFragment.java
+++ app/src/main/java/com/example/mapgame/GameFragment.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class GameFragment extends Fragment {
+    private TextView buttonEventTextView;
+    private GameActivity.ButtonEvent buttonEvent;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_game, container, false);
+        buttonEventTextView = view.findViewById(R.id.buttonEventTextView);
         return view;
     }
+    public void setButtonEvent(GameActivity.ButtonEvent buttonEvent){
+        this.buttonEvent = buttonEvent;
+        buttonEventTextView.setText(buttonEvent.buttonEventString);
+    }
 }
+ 
